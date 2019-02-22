fd59afe43274aa3df7de03caa83f79940553d864
Metahaem <tom.rowson@gmail.com>
2019-02-22 16:15:59 +0000

    added in more route code to app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Route, Switch } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route path="/welcome" component={props=><h1>Welcome to Purrfect Match!</h1>}
-      
+        <Route path="/welcome" component={props=><h1>Welcome to Purrfect Match!</h1>}/>
+        <Route path="/animals" component={props=>
+        <h1>Pet</h1>
+        }
+      />
+
       </div>
-    );
+    )
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+export default Digest
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
