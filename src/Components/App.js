6cbf45d95143bcbfd527316f419bc60cadb56150
Metahaem <tom.rowson@gmail.com>
2019-02-25 18:36:14 +0000

    lots of dependencies added

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import '../App.css';
 import {Route, Switch } from 'react-router-dom'
 import Adopter from './Adopter.js'
+import {Helmet} from "react-helmet";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Route path="/welcome" component={props=><h1>Welcome to Purrfect Match!</h1>}/>
-        <Route path="/adopter" component={props=><Adopter/>}
-      />
+        <Route path="/adopter" component={props=><Adopter/>}/>
 
       </div>
     )
--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Components/App';
+import { render } from 'react-snapshot';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom'
 import 'semantic-ui-css/semantic.min.css'
 import 'semantic-ui-react'
+import { renderToString } from 'react-dom/server';
+import { HeadProvider } from 'react-head';
 
-ReactDOM.render(  
+ReactDOM.render(
   <Router>
     <App />
-  </Router>, document.getElementById('root'));
+  </Router>,
+  document.getElementById('root')
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
