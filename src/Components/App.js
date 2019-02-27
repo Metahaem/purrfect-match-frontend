7c3188b5a5daf2b40037a968939f6bc8d4fb7d8a
Metahaem <tom.rowson@gmail.com>
2019-02-27 10:01:22 +0000

    added drawer to adopter

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,12 +4,15 @@ import {Route, Switch } from 'react-router-dom'
 import Adopter from './Adopter.js'
 import Navbar from './Navbar.js'
 import Welcome from './Welcome.js'
+import ClippedDrawer from './ClippedDrawer.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      
       <Navbar className="navbar navbar-expand-lg bg-secondary fixed-top text-uppercase" id="mainNav"/>
+      <ClippedDrawer />
         <Route path="/welcome" component={ () => <Welcome />}/>
         <Route path="/adopter" component={props=><Adopter/>}/>
         {/* <Route path="/home" component={props=><Home/>} */}
